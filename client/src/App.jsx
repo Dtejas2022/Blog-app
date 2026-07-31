603531e92b04b195daa1394fe56f033d552d1eb7
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2026-07-31 05:22:53 +0000

    learn syntax and then exicute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,48 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/LogIn";
+import Register from "./pages/Register";
+import CreateBlog from "./pages/CreateBlog";
+import EditBlog from "./pages/EditBlog";
+import SingleBlog from "./pages/SingleBlog";
 
 function App() {
 
   return (
-    <>
-      <h1>Blogging Application</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Home/>}
+        />
+
+        <Route 
+          path="/login" 
+          element={<LogIn/>}
+        />
+
+        <Route 
+          path="/register" 
+          element={<Register/>}
+        />
+
+        <Route 
+          path="/create-blog" 
+          element={<CreateBlog/>}
+        />
+
+        <Route 
+          path="/edit-blog/:id" 
+          element={<EditBlog/>}
+        />
+
+        <Route 
+          path="/blog/:id" 
+          element={<SingleBlog/>}
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
